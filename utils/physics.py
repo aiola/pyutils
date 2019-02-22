4fd3a998f1823327f8c23c5f27711cd1a5a2fd1b
Salvatore Aiola <salvatore.aiola@cern.ch>
2019-02-22 11:39:51 +0100

    fix typo

--- utils/physics.py
+++ utils/physics.py
@@ -52,7 +52,7 @@ class MeasuredQuantity(object):
                 exp = precision + 1
             value = self.value / (10**exp)
             error = self.error / (10**exp)
-            result = "({value:.1f} {pm} {error:.1f}) #times 10^{exp}".format(value=value, error=error, exp=exp, pm=plus_minus)
+            result = "({value:.1f} {pm} {error:.1f}) #times 10^{{{exp}}}".format(value=value, error=error, exp=exp, pm=plus_minus)
         else:
             format_string = "{{value:.{prec}f}} {{pm}} {{error:.{prec}f}}".format(prec=abs_precision)
             result = format_string.format(value = self.value, error=self.error, pm=plus_minus)
