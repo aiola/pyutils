e0bc94a2e6cb0666fbb0eef890f22e7ce70adf23
Salvatore Aiola <Salvatore Aiola salvatore.aiola@cern.ch>
2019-06-25 14:04:23 +0200

    user functions

--- pyutils/uproot_utils.py
+++ pyutils/uproot_utils.py
@@ -2,9 +2,11 @@ def readEfficiently(tree, **kwargs):
   dfs = list(tree.iterate(**kwargs))
   return pd.concat(dfs)
 
-def convert2hdf5(treename, filename, dfs):
+def convert2hdf5(treename, filename, dfs, userFunction=[]):
   from pandas import HDFStore
   store = HDFStore(filename, mode='w')
   for df in dfs:
+    for f in userFunction:
+      f(df)
     store.put(treename, df, append=True, format='table', complib='blosc:lz4', complevel=1, data_columns=True)
   store.close()
