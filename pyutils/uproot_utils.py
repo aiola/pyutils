3a2ebe4c43db2b3103b0aaff39df0e51806e8901
Salvatore Aiola <Salvatore Aiola salvatore.aiola@cern.ch>
2019-07-02 11:05:21 +0200

    allow multiple tables in the same HDF store and settable data columns

--- pyutils/uproot_utils.py
+++ pyutils/uproot_utils.py
@@ -1,12 +1,19 @@
+import pandas as pd
+
 def readEfficiently(tree, **kwargs):
   dfs = list(tree.iterate(**kwargs))
   return pd.concat(dfs)
 
-def convert2hdf5(treename, filename, dfs, userFunction=[]):
+def convert2hdf5(dfs, filename, treenames, userFunctions=[], data_columns=True, complib='blosc:lz4', complevel=1):
   from pandas import HDFStore
   store = HDFStore(filename, mode='w')
+  if data_columns == True:
+    data_columns = [True] * len(treenames)
+  elif len(data_columns) != len(treenames or len(userFunctions)):
+    raise ValueError
   for df in dfs:
-    for f in userFunction:
-      f(df)
-    store.put(treename, df, append=True, format='table', complib='blosc:lz4', complevel=1, data_columns=True)
+    for _treename, _function, _data_columns in zip(treenames, userFunctions, data_columns):
+      df_modified = _function(df)
+      if df_modified is not None:
+        store.put(_treename, df_modified, append=True, format='table', complib=complib, complevel=complevel, data_columns=_data_columns)
   store.close()
